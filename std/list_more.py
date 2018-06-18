033b945986051bef74e259cc262e67b9ae9e1f4c
ZJ <zhangjie@megvii.com>
2018-06-19 00:02:03 +0800

    update python file

--- std/list_more.py
+++ std/list_more.py
@@ -22,5 +22,20 @@ print(max(lst2))
 print(sum(lst2))
 
 # 通过 range 创建列表
-lst3 = [value**2 for value in range(1,4)]
+lst3 = [value**2 for value in range(1,6)]
 print(lst3)
+
+# 获取部分列表值 start <= n < last
+print(lst3[0:2])
+# 从头开始
+print(lst3[:2])
+# 从某一位置到结尾
+print(lst3[3:])
+# 获取后几位
+print(lst3[-2:])
+# 获取倒数第几位
+print(lst3[-3])
+
+# Copy list
+lst4 = lst3[:]
+print(lst4)
