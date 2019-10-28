c3ba2dc4c451fba11ad82027bf34886b31b65686
potteryorchid <potteryorchid@163.com>
2019-10-28 16:41:30 +0800

    init

---
+++ com/python/test/test.py
@@ -0,0 +1,19 @@
+import time
+import sys
+from kazoo.client import KazooClient
+
+class ZkClient:
+    def __init__(self):
+        self.zk = KazooClient(hosts=sys.argv[1])
+        self.zk.start()
+        print(self.zk.get("test"))
+    def del_node(self):
+        self.zk.delete(sys.argv[2], recursive=True)
+        print('deleted success')
+    def __del__(self):
+        self.zk.stop()
+if __name__ == '__main__':
+    zkclient = ZkClient()
+    zkclient.del_node()
+    time.sleep(1)
+    del zkclient
