ad8a58df5bbe615b3b656fd554794044341ef020
potteryorchid <potteryorchid@163.com>
2019-12-24 15:33:16 +0800

    bak

---
+++ utils/csv_tools.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.DataFrame
+train = pd.read_csv('../resources/datas/kc_train.csv', names=range(1, 15))
+test = pd.read_csv('../resources/datas/kc_test.csv', names=range(1, 14))
+
+# get some columns
+# df_train_2d = (train.loc[:, [9, 2]])
+
+# remove row
+# df_train_2d.drop([0], inplace=True)
+
+# rename columns
+# df_train_2d.rename(columns={9: None, 2: None}, inplace=True)
+
+# 2d data
+train.to_csv(path_or_buf='../resources/datas/train_2d.csv', columns=[9, 2], encoding='utf-8', header=False, index=False)
+test.to_csv(path_or_buf='../resources/datas/test_2d.csv', columns=[8], encoding='utf-8', header=False, index=False)
+
+# 3d data
+train.to_csv(path_or_buf='../resources/datas/train_3d.csv', columns=[9, 3, 2], encoding='utf-8', header=False, index=False)
+test.to_csv(path_or_buf='../resources/datas/test_3d.csv', columns=[8, 2], encoding='utf-8', header=False, index=False)
